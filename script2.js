dbf38525ede613822d0ab8e35a3fdcb2b43bd251
Souhardh Munnolli <97165471+rmsou27@users.noreply.github.com>
2025-04-12 23:59:57 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             nav.classList.remove('scrolled');
         }
-    });
+    }); 
 
     // Hover effect on menu items
     const navLinks = document.querySelectorAll('#main-nav a');
